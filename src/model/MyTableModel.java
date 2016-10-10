3e2f54ec6444a68417a143ea43163702c78d7058
rpiffeteau <romain.piffeteau@hotmail.fr>
2016-10-10 11:51:40 +0200

    adding mytablemodel

---
+++ src/model/MyTableModel.java
@@ -0,0 +1,13 @@
+package model;
+
+import javax.swing.table.DefaultTableModel;
+
+public class MyTableModel extends DefaultTableModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
