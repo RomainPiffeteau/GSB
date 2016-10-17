8f3b03ed22c1c429ac930bf58c21364f23d6546c
ypçter <et.bourreau@laposte.net>
2016-10-17 14:54:37 +0200

    fix push medicineEffect

--- src/view/MedicineEffect.java
+++ src/view/MedicineEffect.java
@@ -74,11 +74,7 @@ public MedicineEffect()
 	buttonCreation = new JButton("Cr\u00E9er");
 	buttonCreation.setBounds(140, 228, 89, 23);
 	buttonCreation = new JButton("Cr\u00E9er");
-<<<<<<< HEAD
-	buttonCreation.setBounds(243, 228, 89, 23);
-=======
 	buttonCreation.setBounds(239, 228, 89, 23);
->>>>>>> c12e47403d010434f734605001a09dac1b7309b8
 	fenetreEffet.add(buttonCreation);
 	
 	un = new JRadioButton("1");
@@ -91,11 +87,7 @@ public MedicineEffect()
 	fenetreEffet.add(buttonFermer);
 	
 	JButton buttonAnnuler = new JButton("Annuler");
-<<<<<<< HEAD
-	buttonAnnuler.setBounds(144, 228, 89, 23);
-=======
 	buttonAnnuler.setBounds(140, 228, 89, 23);
->>>>>>> c12e47403d010434f734605001a09dac1b7309b8
 	fenetreEffet.add(buttonAnnuler);
 	
 	JRadioButtonMenuItem rbGroup = new JRadioButtonMenuItem("");
